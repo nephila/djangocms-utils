8103100c5890b4bf6e0a316a15ea35aac0551d77
fivethreeo <oyvind.saltvik@gmail.com>
2011-05-18 17:41:10 +0200

    correct version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='djangocms-utils',
-      version='0.9.2',
+      version='0.9.3',
       description='Utilities for django-cms',
       author='Oyvind Saltvik',
       author_email='oyvind.saltvik@gmail.com',
